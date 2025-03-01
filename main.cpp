4163f68704cecc55c3b5e46711f2ae57f8268a7f
minto0626 <daikituttiminto@gmail.com>
2025-03-02 03:27:35 +0900

    空のウィンドウを作成

--- main.cpp
+++ main.cpp
@@ -1,9 +1,62 @@
-#include <Windows.h>
-#include <iostream>
+#include <windows.h>
 
-int main()
+const unsigned int window_width = 1280;
+const unsigned int window_height = 720;
+
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	printf("test.....");
-	getchar();
+	WNDCLASSW wc = {};
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.hInstance = hInstance;
+	wc.lpszClassName = L"DirectX12";
+	wc.lpfnWndProc = WindowProcedure;
+	wc.hIcon = nullptr;
+
+	if (!RegisterClassW(&wc))
+	{
+		return -1;
+	}
+
+	RECT wrect = { 0, 0, window_width, window_height };
+
+	CreateWindowW(
+		wc.lpszClassName,
+		L"MEngine",
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		200,
+		200,
+		wrect.right - wrect.left,
+		wrect.bottom - wrect.top,
+		nullptr,
+		nullptr,
+		wc.hInstance,
+		nullptr);
+
+	MSG msg = {};
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	UnregisterClassW(wc.lpszClassName, wc.hInstance);
+
+	return 0;
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProcW(hWnd, msg, wp, lp);
+	}
+
 	return 0;
 }
