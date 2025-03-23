231cb64bc7eea51b1526fc4a061dfae8d2f75557
minto0626 <daikimento@icloud.com>
2025-03-23 22:01:12 +0900

    基本的なシェーダーのラッパークラスを実装

---
+++ Shader.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <d3d12.h>
+#include <wrl.h>
+#include "d3dx12.h"
+
+using namespace Microsoft::WRL;
+
+class Shader
+{
+private:
+	ComPtr<ID3DBlob> _blob;
+
+	void Load(const wchar_t* filePath, const char* entryPoint, const char* shaderModel);
+	bool CheckShaderCompileResult(HRESULT result, const wchar_t* filePath, ID3DBlob* errorBlob);
+
+public:
+	void LoadVS(const wchar_t* filePath, const char* entryPoint);
+	void LoadPS(const wchar_t* filePath, const char* entryPoint);
+	void LoadCSO(const wchar_t* csoFilePath);
+
+	D3D12_SHADER_BYTECODE GetBytecode() const { return CD3DX12_SHADER_BYTECODE(_blob.Get()); }
+};
