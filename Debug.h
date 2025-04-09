f09f0bfb50b1ccf95a13e910388a38210ee8c9a4
minto0626 <daikimento@icloud.com>
2025-04-09 23:18:12 +0900

    インテリセンスエンジンの負荷になっていそうな、テンプレートの多数定義を削除

--- Debug.h
+++ Debug.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <string>
-#include <format>
 #include <mutex>
 
 class Debug
@@ -22,23 +21,6 @@ public:
 	static void LogAssert(bool condition, const std::wstring& message);
 	static void LogAssert(bool condition, const std::string& message);
 
-	template<typename... Args>
-	static void LogFormat(const wchar_t* format, Args... args);
-	template<typename... Args>
-	static void LogFormat(const char* format, Args... args);
-	template<typename... Args>
-	static void LogErrorFormat(const wchar_t* format, Args... args);
-	template<typename... Args>
-	static void LogErrorFormat(const char* format, Args... args);
-	template<typename... Args>
-	static void LogWarningFormat(const wchar_t* format, Args... args);
-	template<typename... Args>
-	static void LogWarningFormat(const char* format, Args... args);
-	template<typename... Args>
-	static void LogAssertFormat(bool condition, const wchar_t* format, Args... args);
-	template<typename... Args>
-	static void LogAssertFormat(bool condition, const char* format, Args... args);
-
 public:
 	static void ShowMessageBox(const std::wstring& message, const std::wstring& title);
 	static void ShowMessageBox(const std::string& message, const std::string& title);
@@ -48,7 +30,5 @@ public:
 	static void ShowWarningMessageBox(const std::string& message, const std::string& title);
 
 };
-
-#include "Debug.inl"
 
 inline std::mutex Debug::s_mutex;
