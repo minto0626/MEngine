85c4af5e472e8064d905abd0a1aab9fdbfe0830a
minto0626 <daikimento@icloud.com>
2025-05-18 21:46:19 +0900

    乱数生成器を実装

---
+++ include/Random.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <random>
+
+class Random
+{
+public:
+	/// <summary>
+	/// 整数を[min, max-1]の範囲で生成
+	/// </summary>
+	static int Range(int min, int max);
+
+	/// <summary>
+	/// 浮動小数点を[min, max]の範囲で生成
+	/// </summary>
+	static float Range(float min, float max);
+
+	/// <summary>
+	/// 乱数生成器を特定のシードで初期化
+	/// </summary>
+	static void InitState(unsigned int seed);
+
+private:
+	// 乱数生成用の静的エンジン
+	static std::mt19937 _engine;
+
+};
